8dd5dea11979ff5511f9032436af43428ec7ce04
Andy Kluger <andykluger@gmail.com>
2020-10-24 16:47:18 -0400

    tell sphinx to ignore the gh-pages dir (docs), and git to ignore sphinx pickles (docs/.doctrees)

--- conf.py
+++ conf.py
@@ -61,12 +61,12 @@ release = u'latest'
 language = None
 
 locale_dirs = ['locale/']
-gettext_compact = False 
+gettext_compact = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'docs']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
